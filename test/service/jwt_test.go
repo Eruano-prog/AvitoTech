fb550f245c2be96e8e5434dc56f5f1540ea3a231
Eruano <fdgolovlev@gmail.com>
2025-02-13 14:00:18 +0300

    test: add JWT tests

--- test/service/jwt_test.go
+++ test/service/jwt_test.go
@@ -2,11 +2,88 @@ package service
 
 import (
 	"AvitoTech/internal/service"
-	"go.uber.org/zap/zaptest"
 	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
 )
 
-func TestJWT(t *testing.T) {
-	logger := zaptest.NewTestingWriter(t)
-	jwtService := service.NewJWTService(logger, "Secret")
+func TestJWTService_GenerateToken(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	secret := "my-secret-key"
+	s := service.NewJWTService(logger, secret)
+
+	userID := 123
+	token, err := s.GenerateToken(userID)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+
+	parsedUserID, err := s.VerifyToken(token)
+	assert.NoError(t, err)
+	assert.Equal(t, userID, parsedUserID)
+}
+
+func TestJWTService_VerifyToken_ValidToken(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	secret := "my-secret-key"
+	s := service.NewJWTService(logger, secret)
+
+	userID := 123
+	token, err := s.GenerateToken(userID)
+	assert.NoError(t, err)
+
+	parsedUserID, err := s.VerifyToken(token)
+	assert.NoError(t, err)
+	assert.Equal(t, userID, parsedUserID)
+}
+
+func TestJWTService_VerifyToken_InvalidToken(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	secret := "my-secret-key"
+	s := service.NewJWTService(logger, secret)
+
+	invalidToken := "invalid.token.here"
+	parsedUserID, err := s.VerifyToken(invalidToken)
+
+	assert.Error(t, err)
+	assert.Equal(t, -1, parsedUserID)
+}
+
+func TestJWTService_VerifyToken_InvalidUserIDType(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	secret := "my-secret-key"
+	s := service.NewJWTService(logger, secret)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userID": "not-an-int",
+	})
+	tokenString, err := token.SignedString([]byte(secret))
+	assert.NoError(t, err)
+
+	parsedUserID, err := s.VerifyToken(tokenString)
+	assert.Error(t, err)
+	assert.Equal(t, -1, parsedUserID)
+	assert.Contains(t, err.Error(), "invalid userID type")
+}
+
+func TestJWTService_VerifyToken_ExpiredToken(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	secret := "my-secret-key"
+	s := service.NewJWTService(logger, secret)
+
+	claims := jwt.MapClaims{
+		"userID": 123,
+		"exp":    jwt.NewNumericDate(time.Now().Add(-time.Hour)),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secret))
+	assert.NoError(t, err)
+
+	parsedUserID, err := s.VerifyToken(tokenString)
+	assert.Error(t, err)
+	assert.Equal(t, -1, parsedUserID)
+	assert.Contains(t, err.Error(), "token is expired")
 }
